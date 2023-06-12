3f26d795210c923cfc59d021c0cf4f2316eae802
Ömer Karaçay <omerkaracay@gmail.com>
2023-06-12 09:32:54 +0300

    added story feature

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,36 +1,154 @@
-import {SafeAreaView, ScrollView, FlatList} from "react-native";
-import CardItem from "../componenets/CardItem";
-import { useEffect, useState } from "react";
+import {
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+  Image,
+  Modal,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
+import CardItem from '../componenets/CardItem';
+import {useEffect, useState} from 'react';
 //import CardDetail from "./CardDetailScreen";
 
 export default function HomeScreen({navigation}) {
-const [tweetList, setTweetList] = useState(["11İlk Tweet", "İkinci Tweet"])
+  const [tweetList, setTweetList] = useState(['11İlk Tweet', 'İkinci Tweet']);
 
-useEffect(()=>{
+  const [isStoryModalVisible, setIsStoryModalvisible] = useState(false);
+
+  useEffect(() => {
     //anasayfa tweet verileri getir
-    setTimeout(()=> {
-        let listTweetData = GetTweetList()
-        setTweetList(listTweetData)
-    }, 2000)
+    setTimeout(() => {
+      let listTweetData = GetTweetList();
+      setTweetList(listTweetData);
+    }, 2000);
+  }, []);
 
-}, [])
-
-function GetTweetList(){
-    let tweetData = ["İlk Tweet", "İkinci Tweet", "Üçüncü Tweet", "Dördüncü Tweet", "Beşinci Tweet", "Altıncı Tweet", "Yedinci Tweet", "Sekicinci Tweet", "Dokuzuncu Tweet", "Onuncu Tweet", "Onbirinci Tweet"]
+  function GetTweetList() {
+    let tweetData = [
+      'İlk Tweet',
+      'İkinci Tweet',
+      'Üçüncü Tweet',
+      'Dördüncü Tweet',
+      'Beşinci Tweet',
+      'Altıncı Tweet',
+      'Yedinci Tweet',
+      'Sekicinci Tweet',
+      'Dokuzuncu Tweet',
+      'Onuncu Tweet',
+      'Onbirinci Tweet',
+    ];
     return tweetData;
-}
-    return(
-        <SafeAreaView
-            style={{
-                flex: 1,
-                backgroundColor: "white",
-            }}>
-        <FlatList
-        data={tweetList}
-        renderItem={({item}) => <CardItem navigation={navigation} tweet={item} />}
-        //keyExtractor={item => item.id}
-      />   
+  }
+  return (
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+      }}>
+      <ScrollView
+        style={{paddingBottom: 30, paddingTop: 20, height: 180}}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}>
+            <TouchableOpacity onPress={()=> setIsStoryModalvisible(true)}>
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderWidth: 1,
+            borderColor: '#0164FF',
+            borderRadius: 100,
+            marginHorizontal: 10,
+          }}
+        />
+</TouchableOpacity>
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderWidth: 1,
+            borderColor: '#0164FF',
+            borderRadius: 100,
+            marginHorizontal: 10,
+          }}
+        />
 
-        </SafeAreaView>
-    )
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderWidth: 1,
+            borderColor: '#0164FF',
+            borderRadius: 100,
+            marginHorizontal: 10,
+          }}
+        />
+
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderWidth: 1,
+            borderColor: '#0164FF',
+            borderRadius: 100,
+            marginHorizontal: 10,
+          }}
+        />
+
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderWidth: 1,
+            borderColor: '#0164FF',
+            borderRadius: 100,
+            marginHorizontal: 10,
+          }}
+        />
+
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: 70,
+            height: 70,
+            borderWidth: 1,
+            borderColor: '#0164FF',
+            borderRadius: 100,
+            marginHorizontal: 10,
+          }}
+        />
+      </ScrollView>
+      <FlatList
+        data={tweetList}
+        renderItem={({item}) => (
+          <CardItem navigation={navigation} tweet={item} />
+        )}
+        //keyExtractor={item => item.id}
+      />
+
+      <Modal visible={isStoryModalVisible}>
+        <TouchableOpacity onPress={()=> setIsStoryModalvisible(false)}>
+          <Text style={{
+            fontSize: 30,
+            
+            }}>X</Text>
+        </TouchableOpacity>
+        <Image
+          source={require('./../assets/images/user-img.png')}
+          style={{
+            width: '100%',
+            resizeMode: 'contain',
+            //height: 70,
+
+            marginHorizontal: 10,
+          }}
+        />
+      </Modal>
+    </SafeAreaView>
+  );
 }
